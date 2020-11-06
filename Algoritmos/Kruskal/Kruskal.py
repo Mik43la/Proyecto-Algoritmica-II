1b701e6c2e8b5df7adb02e4b944d00cb32ec580f
Mik43la <60782219+Mik43la@users.noreply.github.com>
2020-11-05 23:57:29 -0400

    Update Kruskal.py

--- Algoritmos/Kruskal/Kruskal.py
+++ Algoritmos/Kruskal/Kruskal.py
@@ -1,9 +1,8 @@
 import ast
-from collections import deque, namedtuple
+
 import networkx as nx
 import matplotlib.pyplot as plt
-import heapq as hq
-import math
+
 
 
 class Graph:
@@ -46,10 +45,11 @@ class Graph:
             parent.append(node)
             rank.append(0)
 
-        while e < self.V - 1:
+        while e < self.V - 1 and i<len(self.graph):
 
             u, v, w = self.graph[i]
             i = i + 1
+
             x = self.find(parent, u)
             y = self.find(parent, v)
 
@@ -59,6 +59,10 @@ class Graph:
                 self.union(parent, rank, x, y)
 
         minimumCost = 0
+        # for i in range(len(result)):
+        #     if self.find(parent,result[i][0]) == self.find(parent, destino):
+        #         ruta.append(result[i])
+
         print("Edges in the constructed MST")
         for u, v, weight in result:
             minimumCost += weight
@@ -67,37 +71,80 @@ class Graph:
 
             print("%d -- %d == %d" % (u, v, weight))
         print("Minimum Spanning Tree", minimumCost)
+        # print(len(result), len(ruta))
 
+
+
+
+
+
+destino = 20
 
 G = nx.Graph()
 
-rf = open("cities.txt", "r")
+rf = open("lugares.txt", "r")
 cities =ast.literal_eval( rf.read())
 rf.close()
-rf = open("coordinates.txt", "r")
+rf = open("coordenadas.txt", "r")
 pos=ast.literal_eval(rf.read())
 rf.close()
 
-rf = open("text.txt", "r")
+rf = open("connections.txt", "r")
 
 n, m = map(int, rf.readline().split())
 
 g = Graph(n + 1)# n+1 para que los txt coincidan
 
 for i in range(m):
-    a, b, peso = map(int, rf.readline().split())
-    g.addEdge(a, b, peso)
+    a, b, peso = map(float, rf.readline().split())
+    g.addEdge(int(a), int(b), peso)
+    g.addEdge(int(b), int(a), peso)
 
 g.KruskalMST()
-G = nx.relabel_nodes(G, cities, copy=False)
-img = plt.imread("mapBolivia.jpg")
+
+img = plt.imread("MapaCarreteraBolivia.PNG")
 fig, ax = plt.subplots(figsize=(15,15))
 
-ax.imshow(img, extent=[-100,10,-100,10])
+ax.imshow(img, extent=[-150,200,-150,200], alpha=0.7)
 
-nx.draw_networkx(G, pos, width=2)
+list_of_nodes = list(G.nodes())
+#
+new_dict = "{"
+new_pos ="{"
+
+for i in range(G.number_of_nodes()):
+    new_dict += str(list_of_nodes[i]) + ':"' + str(cities.get(list_of_nodes[i])) + '",'
+    new_pos += "\"" + str(cities.get(list_of_nodes[i])) + "\":" + str(pos.get(cities.get(list_of_nodes[i]))) + ','
+
+new_dict+= "}"
+new_pos+= '}'
+
+dicty = ast.literal_eval(new_dict)
+poss = ast.literal_eval(new_pos)
+
+
+
+
+nx.relabel_nodes(G, mapping=dicty, copy=False)
+
 labels = nx.get_edge_attributes(G, 'weight')
-nx.draw_networkx_edge_labels(G, pos,edge_labels=labels,rotate=True)
+
+list_of_edges = list(G.edges())
+new_labels = "{"
+
+for i in range(len(list_of_edges)):
+    new_labels += str(list_of_edges[i]) + ': ' + str(labels.get(list_of_edges[i])) + ', '
+
+new_labels += "}"
+
+labls2 = ast.literal_eval(new_labels)
+
+G.remove_node('None')
+
+nx.draw_networkx(G, poss,with_labels=True, node_color="cyan", node_size=80, width=1)
 
 
+labels = nx.get_edge_attributes(G, 'weight')
+
+nx.draw_networkx_edge_labels(G,poss,edge_labels=labels)
 plt.show()
